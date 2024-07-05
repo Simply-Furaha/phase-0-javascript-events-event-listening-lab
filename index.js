6886384ca00ef537f51bb5bbbe9bc3b0a6134c07
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-05 19:05:12 +0300

    Implemting Event and alert

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 function addingEventListener() {
-const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
+  const input = document.getElementById('button');
+  input.addEventListener('click', function (){
+    alert('I was clicked!');
+  });
 }
 
-input.addEventListener('click', clickAlert);
-}
+document.addEventListener('DOMContentLoaded', function() {
+  addingEventListener();
+});
